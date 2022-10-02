1c92cec59f6761146d5c717ecd716bb25dcb42ce
Dmitry <dep1982@mail.ru>
2022-10-02 19:59:40 +0300

    Определение последней цифры

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,6 @@
+﻿string threeDigitNumber = Console.ReadLine();
+Console.WriteLine(threeDigitNumber [2]);
+int number = int.Parse (threeDigitNumber);
+Console.WriteLine(number % 10);
+if (number >= 100 && number <= 999);
+// Без первых 2 строк выдает с ошибкой, с первыми 2мя строками дает 2 ответа - 3 и последнюю цифры. Как сделать чтобы выдавал только последнюю не разобрался
